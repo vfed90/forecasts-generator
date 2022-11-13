48a34176c4033dbdb1252232c893cf0a2f0cc517
vfed90 <fed_v90@mail.ru>
2022-11-13 18:39:33 +0300

    Add hometask_5

--- script.js
+++ script.js
@@ -13,3 +13,60 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const forecastButton = document.querySelector(".forecast-btn");
+const forecastTitle = document.querySelector(".current-forecast h1");
+const forecastSubtitle = document.querySelector(".current-forecast p");
+const forecastWrapper = document.querySelector(".forecasts");
+const forecastItem = document.querySelector("#forecast-item");
+
+const firstForecast = "Сказке быть!";
+const secondForecast = "Живи в удовольствие";
+const thirdForecast = "Найдешь то, что давно потерял";
+const fourthForecast = "Что бы ни происходило, у тебя есть ТЫ";
+const fifthForecast = "Случайная встреча изменит всю твою жизнь";
+
+function generateRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function createForecast() {
+  const forecast = forecastItem.content.cloneNode(true);
+  forecast.querySelector("h3").textContent = forecastTitle.textContent;
+  forecast.querySelector("p").textContent = forecastSubtitle.textContent;
+  return forecast;
+}
+
+forecastButton.addEventListener("click", function () {
+  if (forecastTitle.textContent && forecastSubtitle.textContent) {
+    const forecast = createForecast();
+    forecastWrapper.prepend(forecast);
+  }
+
+  let random = generateRandomValue(0, 5);
+
+  switch (random) {
+    case 0:
+      forecastTitle.textContent = firstForecast;
+      break;
+
+    case 1:
+      forecastTitle.textContent = secondForecast;
+      break;
+
+    case 2:
+      forecastTitle.textContent = thirdForecast;
+      break;
+
+    case 3:
+      forecastTitle.textContent = fourthForecast;
+      break;
+
+    case 4:
+      forecastTitle.textContent = fifthForecast;
+      break;
+  }
+
+  let percent = generateRandomValue(0, 100);
+  forecastSubtitle.textContent = `Вероятность: ${percent}%`;
+});
